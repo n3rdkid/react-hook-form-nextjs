5f46a76c38712e7b8e3cf047085b25e8616b946d
Saurav <sauravads123@gmail.com>
2021-07-30 17:24:58 +0545

    basic setup

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useForm } from "react-hook-form";
+
 
 export default function Home() {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
   return (
     <div className={styles.container}>
       <Head>
@@ -15,6 +19,18 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to Form Test
         </h1>
+        <div style={{ display: "flex" }}>
+          <form style={{ display: "flex", flexDirection: "column", gap: "16px", marginRight: "24px", padding: "24px" }} onSubmit={handleSubmit(onSubmit)}>
+            <input defaultValue="test" {...register("example")} />
+            <input {...register("exampleRequired")} />
+            {errors.exampleRequired && <span>This field is required</span>}
+            <button type="submit">Submit</button>
+          </form>
+          <div>
+            <h3>Outputs</h3>
+            {watch("example")}
+          </div>
+        </div>
       </main>
     </div>
   )
